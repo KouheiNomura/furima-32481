289a4872bc3d398f735674d1e0e636c165713fc2
KouheiNomura <nomunomu1139@gmail.com>
2020-12-29 15:38:39 +0900

    ストロングパラメーターの設定とバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
   validates :email, uniqueness: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
-  validates :birth_date, presence: true
+
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
+
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+      validates :last_name
+      validates :first_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー]+\z/ } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+  end
 end
