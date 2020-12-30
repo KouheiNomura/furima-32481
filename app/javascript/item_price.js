5b3da00a0a222b22ae23382352e7d73e25c0582f
KouheiNomura <nomunomu1139@gmail.com>
2020-12-30 21:18:59 +0900

    手数料と利益の算出機能の実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,12 @@
 window.addEventListener('load', () => {
-  console.log("OK");
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profitDom = document.getElementById("profit");
+    const tax = Math.floor(inputValue/10);
+    const profit = inputValue - tax;
+    addTaxDom.innerHTML = tax;
+    profitDom.innerHTML = profit;
+  })
 });
