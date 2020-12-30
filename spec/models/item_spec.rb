70118c8b14bab11902ca0f3eca88a92115c7ffdf
KouheiNomura <nomunomu1139@gmail.com>
2020-12-30 21:22:54 +0900

    Rubocopの実行

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -18,59 +18,59 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
       it 'nameが空では保存できない' do
-        @item.name = ""
+        @item.name = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Name can't be blank")
       end
       it 'infoが空では保存できない' do
-        @item.info = ""
+        @item.info = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Info can't be blank")
       end
       it 'category_idが空では保存できない' do
-        @item.category_id = ""
+        @item.category_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it 'sales_status_idが空では保存できない' do
-        @item.sales_status_id = ""
+        @item.sales_status_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Sales status can't be blank")
       end
       it 'shopping_fee_status_idが空では保存できない' do
-        @item.shopping_fee_status_id = ""
+        @item.shopping_fee_status_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Shopping fee status can't be blank")
       end
       it 'prefecture_idが空では保存できない' do
-        @item.prefecture_id = ""
+        @item.prefecture_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
       it 'scheduled_delivery_idが空では保存できない' do
-        @item.scheduled_delivery_id = ""
+        @item.scheduled_delivery_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank")
       end
       it 'priceが空では保存できない' do
-        @item.price = ""
+        @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it 'priceが300以上でなければ登録できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
       it 'priceが9999999以下でなければ登録できない' do
-        @item.price = 10000000
+        @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
       it 'priceが半角数字のみでなければ登録できない' do
-        @item.price = "１００００"
+        @item.price = '１００００'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not a number")
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
     end
   end
