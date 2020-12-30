8f9b17d47936e6ded78f4a4e2f1dadb8ed158135
KouheiNomura <nomunomu1139@gmail.com>
2020-12-30 16:27:26 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,23 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :info
+    validates :price
+    with_options numericality: {other_than: 0} do
+      validates :category_id
+      validates :sales_status_id
+      validates :shopping_fee_status_id
+      validates :prefecture_id
+      validates :scheduled_delivery_id
+    end
+  end
+  belongs_to :user
+  has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :sales_status
   belongs_to_active_hash :shopping_fee_status
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :scheduled_delivery
-  belongs_to :user
-  has_one_attached :image
 end
