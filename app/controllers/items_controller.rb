822a6d9257248133bc1ebc2f10eadbdf07244073
KouheiNomura <nomunomu1139@gmail.com>
2021-01-04 21:08:25 +0900

    items_controller.rbの編集
    修正依頼箇所を訂正しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_index, only: :edit
 
@@ -20,14 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -43,7 +42,11 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     redirect_to root_path if current_user.id != @item.user.id
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
