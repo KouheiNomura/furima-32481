07636b4e2e6818b1fe38a1d2a4a71fcee268c9db
KouheiNomura <nomunomu1139@gmail.com>
2021-01-11 00:03:58 +0900

    indexアクションの修正と購入画面へのリンクの指定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,11 @@
 class OrdersController < ApplicationController
 
   def index
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
     @purchase_form = PurchaseForm.new
+  end
+
+  def create
   end
 
 end
