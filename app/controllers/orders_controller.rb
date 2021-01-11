37a17cd3e836a315f3efcaa94f3da40c339810bb
KouheiNomura <nomunomu1139@gmail.com>
2021-01-11 15:35:30 +0900

    フォームの情報を保存するためのコードの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,28 @@
 class OrdersController < ApplicationController
+  before_action :set_item
 
   def index
-    @item = Item.find(params[:item_id])
     @purchase_form = PurchaseForm.new
   end
 
   def create
+    @purchase_form = PurchaseForm.new(purchase_form_params)
+    if @purchase_form.valid?
+      @purchase_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def purchase_form_params
+    params.require(:purchase_form).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 
 end
--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -1,21 +1,18 @@
 class PurchaseForm
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase_record
 
   with_options presence: true do
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :postal_code, format: { with: /\A[0-9]{3}[-][0-9]{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 0 }
     validates :city
     validates :address
-    validates :phone_number, format: { with: /\A[0-9]{,8}\z/ } 
+    validates :phone_number, format: { with: /\A[0-9]{,11}\z/ }
   end
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :prefecture
-
   def save
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number)
-    PurchaseRecord.create(user_id: user_id, item_id: item_id)
+    purchase_record = PurchaseRecord.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, purchase_record_id: purchase_record.id)
   end
 
 end
